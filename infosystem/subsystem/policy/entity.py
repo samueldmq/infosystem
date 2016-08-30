249041e5a5ac7adbc7d6a96dee0bf56016d79b45
Francois Oliveira <francois@objetorelacional.com.br>
2016-08-30 15:18:07 -0300

    Refactor entitys names

--- infosystem/subsystem/policy/entity.py
+++ infosystem/subsystem/policy/entity.py
@@ -1,30 +1,18 @@
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
-
+from sqlalchemy import UniqueConstraint
 from infosystem.common.subsystem import entity
 from infosystem.database import db
 
 
 class Policy(entity.Entity, db.Model):
 
-    attributes = ['id', 'domain_id', 'name', 'url', 'method']
+    attributes = ['id', 'domain_id', 'capability_id', 'role_id']
     domain_id = db.Column(db.CHAR(32), db.ForeignKey("domain.id"), nullable=False)
-    name = db.Column(db.String(30), nullable=False)
-    url = db.Column(db.String(100), nullable=False)
-    method = db.Column(db.String(10), nullable=False)
+    capability_id = db.Column(db.CHAR(32), db.ForeignKey("capability.id"), nullable=False)
+    role_id = db.Column(db.CHAR(32), db.ForeignKey("role.id"), nullable=False)
+    UniqueConstraint('domain_id', 'capability_id', 'role_id', name='capability_uk')
 
-    def __init__(self, id, domain_id, name, url, method):
+    def __init__(self, id, domain_id, capability_id, role_id):
         super(Policy, self).__init__(id)
         self.domain_id = domain_id
-        self.name = name
-        self.url = url
-        self.method = method
+        self.capability_id = capability_id
+        self.role_id = role_id
