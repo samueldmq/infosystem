39029c2e4c8cc75b6647e2608b2cf7f71be372ac
Francois Oliveira <francois@objetorelacional.com.br>
2016-09-14 06:33:30 -0300

    Add bypass attribute to policy entity Add composite unique key for role and user using domain

--- infosystem/subsystem/role/entity.py
+++ infosystem/subsystem/role/entity.py
@@ -1,13 +1,15 @@
+from sqlalchemy import UniqueConstraint
 from infosystem.common.subsystem import entity
 from infosystem.database import db
 
 
 class Role(entity.Entity, db.Model):
 
-    attributes = ['id', 'active', 'name', 'domain_id']
-    active = db.Column(db.Boolean(), nullable=False)
-    name = db.Column(db.String(80), nullable=False, unique=True)
+    attributes = ['id', 'name', 'active']
     domain_id = db.Column(db.CHAR(32), db.ForeignKey("domain.id"), nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    active = db.Column(db.Boolean(), nullable=False)
+    UniqueConstraint('domain_id', 'name', name='role_name_uk')
 
     def __init__(self, id, name, domain_id, active=True):
         super(Role, self).__init__(id)
--- infosystem/subsystem/user/entity.py
+++ infosystem/subsystem/user/entity.py
@@ -10,18 +10,21 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from sqlalchemy import UniqueConstraint
 from infosystem.common.subsystem import entity
 from infosystem.database import db
 
 
 class User(entity.Entity, db.Model):
 
-    attributes = ['id', 'domain_id', 'name', 'email', 'active']
-    name = db.Column(db.String(80), nullable=False, unique=True)
+    attributes = ['id', 'name', 'email', 'active']
     domain_id = db.Column(db.CHAR(32), db.ForeignKey("domain.id"), nullable=False)
-    email = db.Column(db.String(80), nullable=False, unique=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
     password = db.Column(db.String(80), nullable=False)
     active = db.Column(db.Boolean(), nullable=False)
+    UniqueConstraint('domain_id', 'name', name='user_name_uk')
+    UniqueConstraint('domain_id', 'email', name='user_email_uk')
 
     def __init__(self, id, name, domain_id, email, password, active=True):
         super(User, self).__init__(id)
