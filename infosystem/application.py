ad4b41904205b15c186c08b1220f3448565eb014
Samuel de Medeiros Queiroz <samueldmq@gmail.com>
2016-09-15 11:30:52 -0300

    Initial bootstrap commit (listing capabiltiies)

--- infosystem/application.py
+++ infosystem/application.py
@@ -1,19 +1,53 @@
 import flask
 import os
 import uuid
+from flask import url_for
+import urllib
+import re
 
 from infosystem.common import authorization
 from infosystem import database
 from infosystem import system as system_module
 
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_folder=None)
 app.config['BASEDIR'] = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SERVER_NAME'] = 'infosystem.com'
 
 system = system_module.System()
 
 database.db.init_app(app)
+
+for subsystem in system.subsystems.values():
+    app.register_blueprint(subsystem)
+
+
+def bootstrap(app, system):
+    output = []
+    for rule in app.url_map.iter_rules():
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = '<' + arg +  '>'
+
+        url = urllib.parse.unquote(url_for(rule.endpoint, **options))
+        url = re.sub(".*" + app.config['SERVER_NAME'],"",url)
+        methods = rule.methods
+        try:
+            # TODO(samueldmq): revisit this. Don't we really want HEAD ?
+            methods.remove('HEAD')
+        except KeyError:
+            pass
+
+        try:
+            methods.remove('OPTIONS')
+        except KeyError:
+            pass
+
+        for method in rule.methods:
+            print(method, url)
+
+
 with app.app_context():
     database.db.create_all()
 
@@ -24,8 +58,8 @@ with app.app_context():
     role = system.subsystems['role'].manager.create(data={'domain_id': domain.id, 'name':'admin'})
     system.subsystems['grant'].manager.create(data={'user_id': user.id, 'role_id':role.id})
 
-for subsystem in system.subsystems.values():
-    app.register_blueprint(subsystem)
+    bootstrap(app, system)
+
 
 def protect():
     return authorization.protect(system)
