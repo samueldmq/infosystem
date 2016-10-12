959858cb93ed6134da00d0099b4e78a9799f65fb
Samuel de Medeiros Queiroz <samueldmq@gmail.com>
2016-10-12 19:47:03 -0300

    Add policy subsystem

---
+++ infosystem/subsystem/policy/__init__.py
@@ -0,0 +1,4 @@
+from infosystem.common import subsystem
+from infosystem.subsystem.policy import resource
+
+subsystem = subsystem.Subsystem(resource=resource.Policy)
---
+++ infosystem/subsystem/policy/resource.py
@@ -0,0 +1,14 @@
+from infosystem.common.subsystem import entity
+from infosystem.database import db
+
+
+class Policy(entity.Entity, db.Model):
+
+    attributes = ['id', 'capability_id', 'role_id']
+    capability_id = db.Column(db.CHAR(32), db.ForeignKey("capability.id"), nullable=False)
+    role_id = db.Column(db.CHAR(32), db.ForeignKey("role.id"), nullable=False)
+
+    def __init__(self, id, capability_id, role_id):
+        super(Grant, self).__init__(id)
+        self.user_id = user_id
+        self.role_id = role_id
