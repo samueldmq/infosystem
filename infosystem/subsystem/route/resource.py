86cfa4e3b60962fa04185efb6500fa6a6c0f4880
Francois Oliveira <francois@objetorelacional.com.br>
2016-10-19 10:27:28 -0300

    Change admin attribute to sysadmin

--- infosystem/subsystem/route/resource.py
+++ infosystem/subsystem/route/resource.py
@@ -7,19 +7,19 @@ class Route(entity.Entity, db.Model):
 
     # TODO(samueldmq): recheck string lengths for below attributes
     # TODO(samueldmq): add an 'active' attribute
-    attributes = ['id', 'name', 'url', 'method', 'admin', 'bypass']
+    attributes = ['id', 'name', 'url', 'method', 'sysadmin', 'bypass']
     name = db.Column(db.String(20), nullable=False)
     url = db.Column(db.String(80), nullable=False)
     method = db.Column(db.String(10), nullable=False)
-    admin = db.Column(db.Boolean(), nullable=False)
+    sysadmin = db.Column(db.Boolean(), nullable=False)
     bypass = db.Column(db.Boolean(), nullable=False)
     __table_args__ = (UniqueConstraint('url', 'method', name='route_uk'),)
 
-    def __init__(self, id, name, url, method, admin=False, bypass=False):
+    def __init__(self, id, name, url, method, sysadmin=False, bypass=False):
         self.id = id
         self.name = name
         self.url = url
         self.method = method
         self.method = method
         self.bypass = bypass
-        self.admin = admin
+        self.sysadmin = sysadmin
