f903f40613d83513f48923f91cf03bc1c606d9f1
Samuel de Medeiros Queiroz <samueldmq@gmail.com>
2016-10-19 10:16:45 -0300

    Release 0.1.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'infosystem',
-    version = '0.1.6',
+    version = '0.1.7',
     summary = 'Infosystem Framework',
     url = 'https://github.com/samueldmq/infosystem',
     author = 'Samuel de Medeiros Queiroz',
