1b8c5a1f667d490d61e9e8b62e3f1720e29e6224
Francois Oliveira <francois@objetorelacional.com.br>
2016-12-06 12:04:35 -0300

    Change tablename of File resource to solve Oracle compatibility

--- infosystem/subsystem/file/resource.py
+++ infosystem/subsystem/file/resource.py
@@ -7,6 +7,7 @@ class File(entity.Entity, db.Model):
     attributes = ['id', 'domain_id', 'name']
     domain_id = db.Column(db.CHAR(32), db.ForeignKey("domain.id"), nullable=True)
     name = db.Column(db.String(100), nullable=True)
+    __tablename__ = 'Files' # This is required because Oracle dont accept FILE for table name
 
     def __init__(self, id, domain_id, name):
         super(File, self).__init__(id)
