a59580555f6c20c867c5caad469eb508bb34be2f
Samuel de Medeiros Queiroz <samueldmq@gmail.com>
2017-04-29 10:43:31 -0300

    Cleaning linting errors in setup,py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = 'infosystem',
-    version = '0.1.20',
-    summary = 'Infosystem Framework',
-    url = 'https://github.com/samueldmq/infosystem',
-    author = 'Samuel de Medeiros Queiroz, Francois Oliveira',
-    author_email = 'samueldmq@gmail.com, oliveira.francois@gmail.com',
-    license = 'Apache-2',
-    packages = find_packages(exclude=["tests"])
-    )
+    name='infosystem',
+    version='0.1.20',
+    summary='Infosystem Framework',
+    url='https://github.com/samueldmq/infosystem',
+    author='Samuel de Medeiros Queiroz, Francois Oliveira',
+    author_email='samueldmq@gmail.com, oliveira.francois@gmail.com',
+    license='Apache-2',
+    packages=find_packages(exclude=["tests"])
+)
