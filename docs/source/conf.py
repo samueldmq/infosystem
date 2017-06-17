d65061f809b01144c28d754e1718089246b36e7e
Samuel de Medeiros Queiroz <samueldmq@gmail.com>
2017-06-17 10:04:08 -0400

    Change theme to default from readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -86,7 +86,13 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'default'
+
+html_context = {'css_files': [
+    'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+    'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+    '_static/theme_override.css',
+]}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
