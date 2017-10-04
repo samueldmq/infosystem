85b34c451fa33d82bf4f8dc51608b08d32773398
Francois Oliveira <francois@objetorelacional.com.br>
2017-10-04 09:00:12 -0300

    Organize routes using pep8

--- infosystem/subsystem/user/router.py
+++ infosystem/subsystem/user/router.py
@@ -1,5 +1,6 @@
 from infosystem.common.subsystem import router
 
+
 class Router(router.Router):
 
     def __init__(self, controller, collection, routes=[]):
@@ -8,7 +9,13 @@ class Router(router.Router):
     @property
     def routes(self):
         return super().routes + [
-            {'action': 'restore', 'method': 'POST', 'url': self.collection_url + '/restore', 'callback': self.controller.restore, 'bypass': True},
-            {'action': 'reset', 'method': 'POST', 'url': self.collection_url + '/reset', 'callback': self.controller.reset},
-            {'action': 'routes', 'method': 'GET', 'url': self.collection_url + '/routes', 'callback': self.controller.routes},
+            {'action': 'restore', 'method': 'POST',
+                'url': self.collection_url + '/restore',
+                'callback': self.controller.restore, 'bypass': True},
+            {'action': 'reset', 'method': 'POST',
+                'url': self.collection_url + '/reset',
+                'callback': self.controller.reset},
+            {'action': 'routes', 'method': 'GET',
+                'url': self.collection_url + '/routes',
+                'callback': self.controller.routes}
         ]
