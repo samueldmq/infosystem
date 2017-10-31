2fe83273ba0eb57298ca865b0e1c973779681e2b
Francois Oliveira <francois@objetorelacional.com.br>
2017-10-31 11:18:00 -0300

    Use default str on get response JSON to avoid Decimal error

--- infosystem/common/subsystem/controller.py
+++ infosystem/common/subsystem/controller.py
@@ -45,7 +45,7 @@ class Controller(object):
 
         response = {self.resource_wrap: entity.to_dict()}
 
-        return flask.Response(response=json.dumps(response),
+        return flask.Response(response=json.dumps(response, default=str),
                               status=200,
                               mimetype="application/json")
 
