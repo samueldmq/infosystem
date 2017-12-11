f465cf1e1cbb093e6e52f8d99442e66e021354b6
Francois Oliveira <francois@objetorelacional.com.br>
2017-12-11 16:58:30 -0300

    Change domain entity constructor

--- infosystem/subsystem/domain/resource.py
+++ infosystem/subsystem/domain/resource.py
@@ -7,11 +7,11 @@ class Domain(entity.Entity, db.Model):
     attributes = ['id', 'active', 'name', 'parent_id']
 
     active = db.Column(db.Boolean(), nullable=False)
-    name = db.Column(db.String(80), nullable=False, unique=True)
+    name = db.Column(db.String(60), nullable=False, unique=True)
     parent_id = db.Column(
         db.CHAR(32), db.ForeignKey("domain.id"), nullable=True)
 
-    def __init__(self, id, name, parent_id=None, active=True):
+    def __init__(self, id, name, active=True, parent_id=None):
         super().__init__(id)
         self.active = active
         self.name = name
