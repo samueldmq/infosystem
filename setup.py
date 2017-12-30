f6989fe0c0af44c5edf4d5a01579c82b3273aa24
Francois Oliveira <francois@objetorelacional.com.br>
2017-12-30 10:16:41 -0300

    Upgrade version and adjust session control doing on last commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='infosystem',
-    version='0.1.37',
+    version='0.1.38',
     summary='Infosystem Framework',
     url='https://github.com/samueldmq/infosystem',
     author='Samuel de Medeiros Queiroz, Francois Oliveira',
