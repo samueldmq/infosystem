87a05df5692ec880a32895d6384ef2a183b0e4eb
Francois Oliveira <francois@objetorelacional.com.br>
2018-12-26 08:41:05 -0300

    Organize attributes on class

--- infosystem/common/subsystem/entity.py
+++ infosystem/common/subsystem/entity.py
@@ -6,24 +6,24 @@ DATETIME_FMT = '%Y-%m-%dT%H:%M:%S.%fZ'
 
 class Entity(object):
 
-    attributes = ['id', 'created_at', 'created_by',
-                  'updated_at', 'updated_by', 'active']
+    attributes = ['id', 'active', 'created_at', 'created_by',
+                  'updated_at', 'updated_by']
 
     id = db.Column(db.CHAR(32), primary_key=True)
+    active = db.Column(db.Boolean())
     created_at = db.Column(db.DateTime)
     created_by = db.Column(db.CHAR(32))
     updated_at = db.Column(db.DateTime)
     updated_by = db.Column(db.CHAR(32))
-    active = db.Column(db.Boolean())
 
     def __init__(self, id, active=True, created_at=None,
                  created_by=None, updated_at=None, updated_by=None):
         self.id = id
+        self.active = active
         self.created_at = created_at
         self.created_by = created_by
         self.updated_at = updated_at
         self.updated_by = updated_by
-        self.active = active
 
     @classmethod
     def embedded(cls):
