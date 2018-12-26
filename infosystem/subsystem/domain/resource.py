cb83a61df5ed82dd9af7ebd3c01d00a3d88e3491
Francois Oliveira <francois@objetorelacional.com.br>
2018-12-26 08:18:32 -0300

    Remove attribute from domain and get from entity

--- infosystem/subsystem/domain/resource.py
+++ infosystem/subsystem/domain/resource.py
@@ -4,10 +4,9 @@ from infosystem.common.subsystem import entity
 
 class Domain(entity.Entity, db.Model):
 
-    attributes = ['active', 'name', 'parent_id']
+    attributes = ['name', 'parent_id']
     attributes += entity.Entity.attributes
 
-    active = db.Column(db.Boolean(), nullable=False)
     name = db.Column(db.String(60), nullable=False, unique=True)
     parent_id = db.Column(
         db.CHAR(32), db.ForeignKey("domain.id"), nullable=True)
@@ -15,7 +14,7 @@ class Domain(entity.Entity, db.Model):
     def __init__(self, id, name, active=True, parent_id=None,
                  created_at=None, created_by=None,
                  updated_at=None, updated_by=None):
-        super().__init__(id, created_at, created_by, updated_at, updated_by)
-        self.active = active
+        super().__init__(id, active, created_at, created_by, 
+                         updated_at, updated_by)
         self.name = name
         self.parent_id = parent_id
