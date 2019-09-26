a4e644b91780ba8293911f0c4c005a2802ec692e
Francois Oliveira <francois@objetorelacional.com.br>
2019-09-26 15:28:47 -0300

    UPdate infosystem version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='infosystem',
-    version='0.1.61',
+    version='0.1.62',
     summary='Infosystem Framework',
     url='https://github.com/samueldmq/infosystem',
     author='Samuel de Medeiros Queiroz, Francois Oliveira',
